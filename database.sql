aafaf248cade85da75e882a703f448d78b81fe74
BlessingOjediran <versatileblessing@gmail.com>
2025-01-01 15:56:11 +0100

    fixed respnsiveness

--- database.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE votes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    contestant_id INT NOT NULL,
-    contestant_name VARCHAR(255) NOT NULL,
-    votes INT NOT NULL,
-    amount DECIMAL(10,2) NOT NULL,
-    vote_date DATETIME NOT NULL
-); 
--- includes/dbh.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-$host = "your_database_host";
-$dbname = "your_database_name";
-$username = "your_database_username";
-$password = "your_database_password";
-?>
